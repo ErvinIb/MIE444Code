a02660a97b35c82ad6ded47561bcc47799564be2
Ervin Ibadula <Ervin Ibadula>
2017-11-07 22:41:57 -0500

    Add project files.

---
+++ MIE444Robot/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace MIE444Robot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.connectButton = new System.Windows.Forms.Button();
+            this.textBoxNorth = new System.Windows.Forms.TextBox();
+            this.textBoxWest = new System.Windows.Forms.TextBox();
+            this.textBoxEast = new System.Windows.Forms.TextBox();
+            this.textBoxSouth = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.textBoxCompass = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.clearOutputButton = new System.Windows.Forms.Button();
+            this.comPortsList = new System.Windows.Forms.ComboBox();
+            this.connectStatus = new System.Windows.Forms.PictureBox();
+            this.mazePic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.connectStatus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mazePic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(891, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(150, 30);
+            this.connectButton.TabIndex = 1;
+            this.connectButton.Text = "Connect to Robot";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // textBoxNorth
+            // 
+            this.textBoxNorth.Location = new System.Drawing.Point(1024, 84);
+            this.textBoxNorth.Name = "textBoxNorth";
+            this.textBoxNorth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNorth.TabIndex = 3;
+            // 
+            // textBoxWest
+            // 
+            this.textBoxWest.Location = new System.Drawing.Point(891, 157);
+            this.textBoxWest.Name = "textBoxWest";
+            this.textBoxWest.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWest.TabIndex = 4;
+            // 
+            // textBoxEast
+            // 
+            this.textBoxEast.Location = new System.Drawing.Point(1157, 157);
+            this.textBoxEast.Name = "textBoxEast";
+            this.textBoxEast.Size = new System.Drawing.Size(100, 20);
+            this.textBoxEast.TabIndex = 5;
+            // 
+            // textBoxSouth
+            // 
+            this.textBoxSouth.Location = new System.Drawing.Point(1024, 230);
+            this.textBoxSouth.Name = "textBoxSouth";
+            this.textBoxSouth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSouth.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(900, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "West Ultrasonic";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1032, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "North Ultrasonic";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1164, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "East Ultrasonic";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1032, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "South Ultrasonic";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(1123, 16);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 11;
+            this.startButton.Text = "Start!";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // textBoxCompass
+            // 
+            this.textBoxCompass.Location = new System.Drawing.Point(1024, 157);
+            this.textBoxCompass.Name = "textBoxCompass";
+            this.textBoxCompass.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCompass.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1024, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Compass Reading";
+            // 
+            // outputBox
+            // 
+            this.outputBox.Location = new System.Drawing.Point(891, 266);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(366, 162);
+            this.outputBox.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(891, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Output Window:";
+            // 
+            // clearOutputButton
+            // 
+            this.clearOutputButton.Location = new System.Drawing.Point(1035, 434);
+            this.clearOutputButton.Name = "clearOutputButton";
+            this.clearOutputButton.Size = new System.Drawing.Size(75, 23);
+            this.clearOutputButton.TabIndex = 16;
+            this.clearOutputButton.Text = "Clear Output";
+            this.clearOutputButton.UseVisualStyleBackColor = true;
+            this.clearOutputButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comPortsList
+            // 
+            this.comPortsList.FormattingEnabled = true;
+            this.comPortsList.Location = new System.Drawing.Point(1047, 18);
+            this.comPortsList.Name = "comPortsList";
+            this.comPortsList.Size = new System.Drawing.Size(70, 21);
+            this.comPortsList.TabIndex = 17;
+            // 
+            // connectStatus
+            // 
+            this.connectStatus.Image = global::MIE444Robot.Properties.Resources.Offline;
+            this.connectStatus.Location = new System.Drawing.Point(894, 48);
+            this.connectStatus.Name = "connectStatus";
+            this.connectStatus.Size = new System.Drawing.Size(30, 30);
+            this.connectStatus.TabIndex = 2;
+            this.connectStatus.TabStop = false;
+            this.connectStatus.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // mazePic
+            // 
+            this.mazePic.Image = global::MIE444Robot.Properties.Resources.Maze;
+            this.mazePic.Location = new System.Drawing.Point(0, 0);
+            this.mazePic.Name = "mazePic";
+            this.mazePic.Size = new System.Drawing.Size(885, 457);
+            this.mazePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.mazePic.TabIndex = 0;
+            this.mazePic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 462);
+            this.Controls.Add(this.comPortsList);
+            this.Controls.Add(this.clearOutputButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.outputBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxCompass);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSouth);
+            this.Controls.Add(this.textBoxEast);
+            this.Controls.Add(this.textBoxWest);
+            this.Controls.Add(this.textBoxNorth);
+            this.Controls.Add(this.connectStatus);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.mazePic);
+            this.Name = "Form1";
+            this.Text = "Robot Interface";
+            ((System.ComponentModel.ISupportInitialize)(this.connectStatus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mazePic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mazePic;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.PictureBox connectStatus;
+        private System.Windows.Forms.TextBox textBoxNorth;
+        private System.Windows.Forms.TextBox textBoxWest;
+        private System.Windows.Forms.TextBox textBoxEast;
+        private System.Windows.Forms.TextBox textBoxSouth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox textBoxCompass;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox outputBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button clearOutputButton;
+        private System.Windows.Forms.ComboBox comPortsList;
+    }
+}
+
---
+++ MIE444Robot/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace MIE444Robot
+{
+    public partial class Form1 : Form
+    {
+        // offsets for ultrasonics
+        const int offsetNorth = 0;
+        const int offsetEast = 0;
+        const int offsetSouth = 0;
+        const int offsetWest = 0;
+
+        // Readings off robot
+        int northUltrasonic = 0;
+        int eastUltrasonic = 0;
+        int southUltrasonic = 0;
+        int westUltrasonic = 0;
+        int compass = 0;
+
+        // distances to move different amounts of zones
+        const int moveOneZoneNorth = 15 - offsetNorth;
+        const int moveOneZoneSouth = 15 - offsetSouth;
+        const int moveOneZoneWest = 15 - offsetWest;
+        const int moveTwoZonesNorth = 45 - offsetNorth;
+        const int moveTwoZonesWest = 45 - offsetWest;
+        const int moveThreeZonesWest = 75 - offsetWest;
+        const int moveFourZonesWest = 105 - offsetWest; // Check if greater than or less than 100
+
+        // thresholds - below this we have a wall
+        const int thresholdNorth = 30 - offsetNorth;
+        const int thresholdEast = 30 - offsetEast;
+        const int thresholdSouth = 30 - offsetSouth;
+        const int thresholdWest = 30 - offsetWest;
+
+        // explicit communication string over serial port
+        // see RFP for zone designations
+        // yes, these can be shortened with a bunch of loops
+        // no, I'm not doing that for readability's sake
+
+        // LEVEL1
+        string zone1 = "D";
+        string zone2 = "D";
+        string zoneCircle = "PW0" + moveOneZoneWest.ToString() + "L000";
+        string zone4 = "PW0" + moveTwoZonesWest.ToString();
+        string zoneSquare = "PS0" + moveOneZoneSouth.ToString() + "L000";
+        // zone 6 = zoneSquare
+        string zone7 = "D";
+        string zoneHexagon = "PW0" + moveOneZoneWest.ToString() + "L000";
+        string zoneTriangle = "PN0" + moveOneZoneNorth.ToString() + "L000";
+        // zone 10 = zoneCircle
+        string zone11 = "PW0" + moveTwoZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        // zone 12 = zoneCircle
+        string zone13 = "PW" + moveFourZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        string zonePentagon = "PN0" + moveOneZoneNorth.ToString() + "L000";
+        // zone 15 = zoneSquare
+        // zone 16 = zonePentagon
+        // zone 17 = zonePentagon
+        // zone 18 = zoneTriangle
+        // zone 19 = zoneCircle
+        string zone20 = "PW0" + moveTwoZonesWest.ToString() + "N0" + moveTwoZonesNorth.ToString();
+        // zone21 = zoneCircle
+        // zone22 = zoneCircle
+        // zone23 = zoneHexagon
+        string zone24 = "PN0" + moveTwoZonesNorth.ToString() + "W" + moveFourZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+
+        // LEVEL2
+        // Everything is unambiguous now
+        // Note: when saying "zone5" as an example, we are actually at zone11 now after moving, and the string represents the remaining path
+        // Case: zoneSquare
+        string zone5 = "PW0" + moveTwoZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        string zone6 = "PW" + moveFourZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        string zone15 = "PW0" + moveTwoZonesWest.ToString() + "N0" + moveTwoZonesNorth.ToString();
+        // Case: zonePentagon
+        string zone14 = "D";
+        string zone16 = "PW0" + moveTwoZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        string zone17 = "PW" + moveFourZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        // Case: zoneTriangle
+        string zone9 = "PW0" + moveTwoZonesWest.ToString();
+        string zone18 = "PN0" + moveOneZoneNorth.ToString();
+        // Case: zoneHexagon
+        string zone8 = "D";
+        string zone23 = "PW" + moveFourZonesWest.ToString() + "N0" + moveTwoZonesNorth.ToString();
+        // Case: zoneCircle
+        string zone3 = "D";
+        string zone10 = "PN0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        string zone12 = "PW0" + moveTwoZonesWest.ToString() + "N0" + moveOneZoneNorth.ToString() + "W0" + moveTwoZonesWest.ToString();
+        string zone19 = "PN0" + moveTwoZonesNorth.ToString();
+        string zone21 = "PW0" + moveTwoZonesWest.ToString() + "N0" + moveTwoZonesNorth.ToString();
+        string zone22 = "PW0" + moveThreeZonesWest.ToString() + "N0" + moveTwoZonesNorth.ToString();
+
+        static SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Find COM ports, add to list
+            foreach ( string s in SerialPort.GetPortNames())
+            {
+                comPortsList.Items.Add(s);
+            }
+
+            WriteOutput("\"Connect to Robot\" then once connection is established, press \"Start!\"");
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            // Set Serial Port Properties - may need to modify later
+            serialPort = new SerialPort();
+            serialPort.PortName = comPortsList.SelectedItem.ToString();
+            serialPort.BaudRate = 9600;
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.None;
+            serialPort.Handshake = Handshake.None;
+            //serialPort.ReadTimeout = 500;
+            //serialPort.WriteTimeout = 500;
+
+            // Open Serial Port
+            serialPort.Open();
+            if ( serialPort.IsOpen)
+            {
+                connectStatus.Image = Properties.Resources.Online;
+                WriteOutput("Connection Successful.");
+            }
+        }
+
+        private void WriteOutput( string output)
+        {
+            outputBox.Text += output + "\n";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Clear output box text
+            outputBox.Text = "";
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                Localize();
+            }
+            else
+            {
+                WriteOutput("Connection is not established.");
+                connectStatus.Image = Properties.Resources.Offline;
+            }
+        }
+
+        private void Localize()
+        {
+            serialPort.WriteLine("L");
+            WriteOutput("Localizing...");
+            string readString = serialPort.ReadLine();
+            WriteOutput(readString);
+            CheckAndSetReadLocation(readString);
+
+            if ( northUltrasonic > thresholdNorth && eastUltrasonic > thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic > thresholdWest)
+            {
+                // No walls
+                serialPort.WriteLine(zone11);
+            }else if (northUltrasonic <= thresholdNorth && eastUltrasonic > thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic > thresholdWest)
+            {
+                // North wall
+                serialPort.WriteLine(zone2);
+            }else if (northUltrasonic > thresholdNorth && eastUltrasonic <= thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic > thresholdWest)
+            {
+                // East wall
+                serialPort.WriteLine(zone13);
+            }else if (northUltrasonic > thresholdNorth && eastUltrasonic > thresholdEast && southUltrasonic <= thresholdSouth && westUltrasonic > thresholdWest)
+            {
+                // South wall
+                serialPort.WriteLine(zone20);
+            }else if (northUltrasonic > thresholdNorth && eastUltrasonic > thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic <= thresholdWest)
+            {
+                // West wall
+                serialPort.WriteLine(zone7);
+            }else if (northUltrasonic <= thresholdNorth && eastUltrasonic > thresholdEast && southUltrasonic <= thresholdSouth && westUltrasonic > thresholdWest)
+            {
+                // North and South wall
+                serialPort.WriteLine(zoneCircle);
+                // MORE
+            }else if (northUltrasonic <= thresholdNorth && eastUltrasonic <= thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic > thresholdWest)
+            {
+                // North and East wall
+                serialPort.WriteLine(zone4);
+            }else if (northUltrasonic <= thresholdNorth && eastUltrasonic > thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic <= thresholdWest)
+            {
+                // North and West wall
+            }else if (northUltrasonic > thresholdNorth && eastUltrasonic <= thresholdEast && southUltrasonic <= thresholdSouth && westUltrasonic > thresholdWest)
+            {
+                // South and East wall
+            }else if (northUltrasonic > thresholdNorth && eastUltrasonic > thresholdEast && southUltrasonic <= thresholdSouth && westUltrasonic <= thresholdWest)
+            {
+                // South and West wall
+            }else if (northUltrasonic > thresholdNorth && eastUltrasonic <= thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic <= thresholdWest)
+            {
+                // East and West wall
+            }else if (northUltrasonic > thresholdNorth && eastUltrasonic <= thresholdEast && southUltrasonic <= thresholdSouth && westUltrasonic <= thresholdWest)
+            {
+                // East, South and West wall
+            }else if (northUltrasonic <= thresholdNorth && eastUltrasonic <= thresholdEast && southUltrasonic > thresholdSouth && westUltrasonic <= thresholdWest)
+            {
+                // North, East and West wall
+            }
+        }
+
+        private void CheckAndSetReadLocation(string message)
+        {
+            // RN000E000S000W000C000
+            // 01234
+            if (message[0] == 'R' && message.Length == 21)
+            {
+                // Parse message
+                northUltrasonic = Int32.Parse(message.Substring(2,3));
+                eastUltrasonic = Int32.Parse(message.Substring(6, 3));
+                southUltrasonic = Int32.Parse(message.Substring(10, 3));
+                westUltrasonic = Int32.Parse(message.Substring(14, 3));
+                compass = Int32.Parse(message.Substring(18, 3));
+
+                // Set text boxes
+                textBoxNorth.Text = northUltrasonic.ToString();
+                textBoxEast.Text = eastUltrasonic.ToString();
+                textBoxSouth.Text = southUltrasonic.ToString();
+                textBoxWest.Text = westUltrasonic.ToString();
+                textBoxCompass.Text = compass.ToString();
+            }else
+            {
+                WriteOutput("Invalid message format");
+            }
+        }
+    }
+}
